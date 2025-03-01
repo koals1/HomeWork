3a4d86a040646ef174af101dbbe9b067429b4d07
ASUS <ASUS@DESKTOP-7ONLO00>
2025-03-01 18:02:13 +0200

    HomeWork

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Введите число от 1 до 100: ");
+        int number;
+
+       
+        i
+         
+            if (number < 1 || number > 100)
+            {
+                Console.WriteLine("Ошибка! Число должно быть в диапазоне от 1 до 100.");
+            }
+            else
+            {
+               
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    
+                    Console.WriteLine(number);
+                }
+            }
+        
+       
+    }
+}
