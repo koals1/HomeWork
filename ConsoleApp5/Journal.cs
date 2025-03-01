216a5e02e66a93bcd4b8147e75a4ca17b72541c0
ASUS <ASUS@DESKTOP-7ONLO00>
2025-03-01 18:28:44 +0200

    Journal

---
+++ ConsoleApp5/Journal.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassWork
+{
+    internal class Journal
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Journal(string name, int year, string descpription, string phone, string email)
+        {
+            Name = name;
+            Year = year;
+            Email = email;
+            Description = descpription;
+            Phone = phone;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nAdress: {Year}\nDescription: {Description}\nPhone: {Phone}\nEmail: {Email}";
+        }
+    }
+}
--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            //Task 1
             Shop shop1 = new Shop("Rozetka", "Kiev", "descpription", "+38097...", "shop@gmail.com");
             shop1.Name = "Rozetka";
             shop1.Adress = "Odesa";
@@ -18,8 +20,13 @@ namespace ConsoleApp5
             shop1.Phone = "phone number";
             shop1.Email = "email.com";
             Console.WriteLine(shop1);
+            //Task 2
+            Console.WriteLine();
+            Journal journal1 = new Journal("Forbs", 2010, "descpription", "+38097....", "forbs@gmail.com");
+            Console.WriteLine(journal1);
 
 
         }
+
     }
 }
