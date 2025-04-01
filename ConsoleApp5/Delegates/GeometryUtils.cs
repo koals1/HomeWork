cdfbc354e29283881c0e3e78fbf121c7af706253
ASUS <ASUS@DESKTOP-7ONLO00>
2025-04-01 08:37:13 +0300

    Delegates

---
+++ ConsoleApp5/Delegates/GeometryUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class GeometryUtils
+{
+    public static void ShowTime()
+    {
+        Console.WriteLine("Текущее время: " + DateTime.Now.ToString("HH:mm:ss"));
+    }
+
+    public static void ShowDate()
+    {
+        Console.WriteLine("Текущая дата: " + DateTime.Now.ToString("dd.MM.yyyy"));
+    }
+
+    public static void ShowDayOfWeek()
+    {
+        Console.WriteLine("День недели: " + DateTime.Now.DayOfWeek);
+    }
+
+    public static double TriangleArea(double a, double b, double c)
+    {
+        double p = (a + b + c) / 2;
+        double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
+
+    public static double RectangleArea(double width, double height)
+    {
+        double area = width * height;
+        return area;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Action showTime = GeometryUtils.ShowTime;
+        Action showDate = GeometryUtils.ShowDate;
+        Action showDayOfWeek = GeometryUtils.ShowDayOfWeek;
+
+        Func<double, double, double, double> triangleArea = GeometryUtils.TriangleArea;
+        Func<double, double, double> rectangleArea = GeometryUtils.RectangleArea;
+
+        showTime();
+        showDate();
+        showDayOfWeek();
+
+        double triangle = triangleArea(3, 4, 5);
+        Console.WriteLine("Площадь треугольника (3, 4, 5): " + triangle);
+
+        double rectangle = rectangleArea(5, 10);
+        Console.WriteLine("Площадь прямоугольника (5, 10): " + rectangle);
+    }
+}
---
+++ ConsoleApp5/Delegates/NumberFilter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+delegate bool NumberFilter(int number);
+
+class ArrayUtils
+{
+    public static int[] Filter(int[] array, NumberFilter filter)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (filter(array[i]))
+            {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (filter(array[i]))
+            {
+                result[index] = array[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    public static bool IsOdd(int number)
+    {
+        return number % 2 != 0;
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool IsFibonacci(int number)
+    {
+        if (number < 0)
+        {
+            return false;
+        }
+
+        int a = 0, b = 1;
+
+        while (a < number)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return a == number;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 8, 13, 21, 34, 55, 89, 144 };
+
+        int[] evens = ArrayUtils.Filter(numbers, ArrayUtils.IsEven);
+        int[] odds = ArrayUtils.Filter(numbers, ArrayUtils.IsOdd);
+        int[] primes = ArrayUtils.Filter(numbers, ArrayUtils.IsPrime);
+        int[] fibonacciNumbers = ArrayUtils.Filter(numbers, ArrayUtils.IsFibonacci);
+
+        Console.WriteLine("Четные: " + string.Join(", ", evens));
+        Console.WriteLine("Нечетные: " + string.Join(", ", odds));
+        Console.WriteLine("Простые: " + string.Join(", ", primes));
+        Console.WriteLine("Фибоначчи: " + string.Join(", ", fibonacciNumbers));
+    }
+}
